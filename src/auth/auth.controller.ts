27d0dd31a8f8bb328a008f30235c0659846af868
yogyy <yogimr261@gmail.com>
2024-01-10 22:08:06 +0700

    feat: CRUD

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
+
+  @Post('signup')
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
+}
