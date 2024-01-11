21e61e106358f3ca06fab13ed025cae29080ad62
yogyy <yogimr261@gmail.com>
2024-01-11 23:07:35 +0700

    Complete e2e testing
    add bookmark testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,6 +5,7 @@ import { PrismaService } from '../src/prisma/prisma.service';
 import { AuthDto } from '../src/auth/dto';
 import * as pactum from 'pactum';
 import { EditUserDto } from 'src/user/dto';
+import { CreateBookmarkDto, EditBookmarkDto } from 'src/bookmark/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -128,11 +129,108 @@ describe('App e2e', () => {
       });
     });
   });
-  describe('Bookmark', () => {
-    describe('Create bookmark', () => {});
-    describe('Get bookmarks', () => {});
-    describe('Get bookmarks by id', () => {});
-    describe('Edit bookmarks by id', () => {});
-    describe('Delete bookmarks by id', () => {});
+
+  describe('bookmarks', () => {
+    describe('Get empty bookmark', () => {
+      it('should get bookmarks', () => {
+        return pactum
+          .spec()
+          .get('/bookmarks')
+          .withHeaders({ Authorization: 'Bearer $S{userAt}' })
+          .expectStatus(200)
+          .expectBody([]);
+      });
+    });
+  });
+
+  describe('Create bookmark', () => {
+    const dto: CreateBookmarkDto = {
+      title: 'First Bookmark',
+      link: 'https://www.youtube.com/watch?v=d6WC5n9G_sM',
+    };
+    it('should create bookmark', () => {
+      return pactum
+        .spec()
+        .post('/bookmarks')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        })
+        .withBody(dto)
+        .expectStatus(201)
+        .stores('bookmarkId', 'id');
+    });
+  });
+
+  describe('Get bookmarks', () => {
+    it('should get bookmarks', () => {
+      return pactum
+        .spec()
+        .get('/bookmarks')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        })
+        .expectStatus(200)
+        .expectJsonLength(1);
+    });
+  });
+
+  describe('Get Bookmark by id', () => {
+    it('should get bookmarks by id', () => {
+      return pactum
+        .spec()
+        .get('/bookmarks/{id}')
+        .withPathParams('id', '$S{bookmarkId}')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        })
+        .expectStatus(200)
+        .expectBodyContains('$S{bookmarkId}');
+    });
+  });
+
+  describe('Edit bookmark by id', () => {
+    const dto: EditBookmarkDto = {
+      title: "Guns N' Roses - Sweet Child O' Mine (Official Music Video)",
+      description:
+        'https://www.youtube.com/watch?v=oCtYekG1qBc&ab_channel=BBCRadio1',
+    };
+    it('should edit bookmarks by id', () => {
+      return pactum
+        .spec()
+        .patch('/bookmarks/{id}')
+        .withPathParams('id', '$S{bookmarkId}')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        })
+        .withBody(dto)
+        .expectStatus(200)
+        .expectBodyContains('$S{bookmarkId}')
+        .expectBodyContains(dto.title)
+        .expectBodyContains(dto.description);
+    });
+  });
+
+  describe('Delete bookmark by id', () => {
+    it('should edit bookmarks by id', () => {
+      return pactum
+        .spec()
+        .delete('/bookmarks/{id}')
+        .withPathParams('id', '$S{bookmarkId}')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}',
+        })
+        .expectStatus(204);
+    });
+  });
+
+  it('should get empty bookmarks', () => {
+    return pactum
+      .spec()
+      .get('/bookmarks')
+      .withHeaders({
+        Authorization: 'Bearer $S{userAt}',
+      })
+      .expectStatus(200)
+      .expectJsonLength(0);
   });
 });
