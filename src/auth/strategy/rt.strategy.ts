e37701268c8bf6ddba20c5a13e3d926550585491
yogyy <yogimr261@gmail.com>
2024-02-17 16:38:18 +0700

    feat: add refresh token

---
+++ src/auth/strategy/rt.strategy.ts
@@ -0,0 +1,33 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get<string>('RT_SECRET'),
+      passReqToCallback: true,
+    });
+  }
+
+  validate(
+    req: Request,
+    payload: { sub: number; email: string } & { refreshToken: string },
+  ) {
+    const refreshToken = req
+      ?.get('authorization')
+      ?.replace('Bearer', '')
+      .trim();
+
+    if (!refreshToken) throw new ForbiddenException('Refresh token malformed');
+
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
