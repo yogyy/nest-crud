ff47991f00b7e7239d51176302d8499d8e948a57
yogyy <yogimr261@gmail.com>
2024-04-29 22:46:57 +0700

    feat: add global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app
+    .useGlobalPipes(new ValidationPipe({ whitelist: true }))
+    .setGlobalPrefix('api');
   await app.listen(4000);
 }
 bootstrap();
